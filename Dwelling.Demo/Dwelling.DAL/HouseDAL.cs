82773d55e8fffa416225ff81b03995721d8e6af5
dlbayue6 <45560387+dlbayue6@users.noreply.github.com>
2018-12-04 19:46:43 +0800

    首个版本
    第一次提交

---
+++ Dwelling.Demo/Dwelling.DAL/HouseDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwelling.DAL
+{
+    using Dwelling.IDAL;
+    public class HouseDAL:IHouseDAL
+    {
+        public int Add()
+        {
+            return 1;
+        }
+    }
+}
